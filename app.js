f3641859b3bb9085b71ab04281d4571e2a2c78e9
juljasong <juljasong@gmail.com>
2020-02-17 23:36:27 +0900

    #2.10 MVC Pattern part Two

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express(); // app 변수 선언하여 express 실행
 
@@ -18,8 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // 옵션 X
 app.use(morgan("dev")); // 옵션 지정
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes"; // .. : 이전 디렉토리
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req, res) => res.send('Home'));
+globalRouter.get(routes.join, (req, res) => res.send('Join'));
+globalRouter.get(routes.login, (req, res) => res.send('LogIn'));
+globalRouter.get(routes.logout, (req, res) => res.send('LogOut'));
+globalRouter.get(routes.search, (req, res) => res.send('Search'));
+
 
 export default globalRouter; 
